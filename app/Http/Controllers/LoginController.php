b0b5f3bf1c3919b8932e9e3d8f1a7f19ca7dc27a
zhaozeyu <18645124160@163.com>
2017-08-05 18:13:02 +0800

    上线

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Storage;
 use SoapClient;
-use App\Serives\WsMessageSend;
+use App\Services\WsMessageSend;
 
 class LoginController extends Controller
 {   
